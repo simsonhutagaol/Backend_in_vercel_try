e2781fc3f9ad76b1316d5bb9d4d55fc4331470e8
simsonhutagaol <simsonhutagaol58@gmail.com>
2024-08-14 23:51:01 +0700

    add some endpoint

--- api/index.js
+++ api/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api", (req, res) => res.send("API on Vercel"));
+app.get("/api/v1", (req, res) => res.send("API v1 on Vercel"));
+app.get("/api/v1/express", (req, res) => {
+  req.query.name
+    ? res.send(`Hello, ${req.query.name}!`)
+    : res.send("Hello, Express!");
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
