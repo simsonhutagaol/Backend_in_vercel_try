b2e3adc877ff82c7c6f659d790bd9ca3d2b2dcd3
simsonhutagaol <simsonhutagaol58@gmail.com>
2024-08-15 10:56:08 +0700

    change res

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
+const { info } = require("console");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get("/api", (req, res) => res.send("API on Vercel"));
-app.get("/api/v1", (req, res) => res.send("API v1 on Vercel"));
+app.get("/", (req, res) => res.json({ info: "Express on Vercel" }));
+app.get("/api", (req, res) => res.json({ Info: "API on Vercel" }));
+app.get("/api/v1", (req, res) => res.json({ info: "API v1 on Vercel" }));
 app.get("/api/v1/express", (req, res) => {
   req.query.name
-    ? res.send(`Hello, ${req.query.name}!`)
-    : res.send("Hello, Express!");
+    ? res.json({
+        Sapaan: `Hello, ${req.query.name}!`,
+        info: "Nama diambil dari query",
+      })
+    : res.json({
+        error: "Query tidak ditemukan / tidak ada nama yang diinputkan",
+      });
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
